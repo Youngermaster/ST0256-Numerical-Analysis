849987fe13e98d311167c42539b27e1f4b28be9a
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-05-31 19:44:55 -0500

    Python linting

--- Python/fijo2.py
+++ Python/fijo2.py
@@ -1,32 +1,35 @@
 from math import *
+
+
 def g(x):
 
-    y=sqrt(10/(x+4))
+    y = sqrt(10/(x+4))
 
     return y
 
-p0=2
 
-tol=0.001
+p0 = 2
 
-n0=50
+tol = 0.001
 
-i=1
+n0 = 50
 
-while i<=n0:
+i = 1
 
-    p=g(p0)
+while i <= n0:
 
-    if abs(p-p0)<tol:
+    p = g(p0)
 
-        print("El punto fijo es ",p," despues de ",i," iteraciones")
+    if abs(p-p0) < tol:
+
+        print("El punto fijo es ", p, " despues de ", i, " iteraciones")
 
         break
 
-    i=i+1
+    i = i+1
 
-    p0=p
+    p0 = p
 
-if i>n0:
+if i > n0:
 
     print("El metodo no converge ")
