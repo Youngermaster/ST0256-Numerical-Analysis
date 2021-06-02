9dacbbf515ac319adbfc1949618b81fc9e0e5238
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-06-02 17:00:02 -0500

    Second Test added

--- Python/newtonRaphson.py
+++ Python/newtonRaphson.py
@@ -2,29 +2,6 @@ import numpy as np
 from math import *
 import sympy as sp
 
-"""
-# Datos necesarios para la ejecucion
-ecuacion = input("Ingrese la ecuacion: ")
-x0 = float(input("Ingrese el punto inicial: "))
-tol = float(input("Ingrese la tolerancia: "))
-nmax = float(input("ingrese el numero maximo de iteracciones: "))
-
-
-x = sp.Symbol('x')
-
-# Funcion a evaluar
-def f(x):
-    return eval(ecuacion)
-
-
-# Evaluacionde la derivada de la funcion
-def df(x):
-    return eval(str(dev()))
-
-# Derivada de la funcion a evaluar
-def dev():
-    return sp.diff(ecuacion, x)
-"""
 
 
 
@@ -58,6 +35,13 @@ def f_test_1(x):
 def df_test_1(x):
     return (3*x**2) - (2*x)
 
+def f_test_2(x):
+    return (2 * x**3) - (2 * x) - 5
+
+
+def df_test_2(x):
+    return (6 * x**2) - 2
+
 
 if __name__ == "__main__":
     x0 = -20
@@ -68,9 +52,41 @@ if __name__ == "__main__":
 
 
 # Tests
-def test_newton_raphson():
+def test_1():
     x0 = -20
     tol = 0.00001
     nmax = 20
     error = 100
     assert newtonRaphson(x0, tol, nmax, error, True, f_test_1, df_test_1) == -1.000000000000011
+
+def test_2():
+    x0 = 2
+    tol = 0.00001
+    nmax = 20
+    error = 100
+    assert newtonRaphson(x0, tol, nmax, error, True, f_test_2, df_test_2) == 1.6005985449336209
+
+
+"""
+# Datos necesarios para la ejecucion
+ecuacion = input("Ingrese la ecuacion: ")
+x0 = float(input("Ingrese el punto inicial: "))
+tol = float(input("Ingrese la tolerancia: "))
+nmax = float(input("ingrese el numero maximo de iteracciones: "))
+
+
+x = sp.Symbol('x')
+
+# Funcion a evaluar
+def f(x):
+    return eval(ecuacion)
+
+
+# Evaluacionde la derivada de la funcion
+def df(x):
+    return eval(str(dev()))
+
+# Derivada de la funcion a evaluar
+def dev():
+    return sp.diff(ecuacion, x)
+"""
