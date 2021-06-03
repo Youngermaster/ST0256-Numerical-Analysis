827d79d2305d9bf1a6f50d07273ee3b968bb4853
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-06-02 20:32:32 -0500

    Secant Test added

--- Python/secant.py
+++ Python/secant.py
@@ -1,5 +1,51 @@
 import numpy as np
 import sympy as sp
+
+# Global variables
+NMAX = 100
+ERROR = 100
+TOLERANCE = 0.00001
+
+
+def secant(x0, x1, tol, nmax, error, f, test):
+    ni = 0
+    if not test:
+        print("# iter\t\t x \t\t f(x) \t\t error")
+        print("{0:d}\t{1:8.6f}\t{2:8.6f}\t{3:8.6f}".format(ni, x1, f(x1), error))
+
+    # Ejecucion del proceso
+    while error > tol and ni < nmax:
+        x2 = x1 - (f(x1) * (x1 - x0)) / (f(x1) - f(x0))
+        ni += 1
+        error = abs(x2 - x1)
+        if not test:
+            print("{0:d}\t{1:8.4f}\t{2:8.6f}\t{3:8.6f}".format(ni, x1, x2, error))
+        x0 = x1
+        x1 = x2
+
+    if not test:
+        print("La raiz es: ", x1)
+    else:
+        return x1
+
+
+def f_test_1(x):
+    return x ** 3 + 1
+
+
+if __name__ == "__main__":
+    x0 = 0
+    x1 = 2
+    secant(x0, x1, TOLERANCE, NMAX, ERROR, f_test_1, False)
+
+
+# Tests
+def test_1():
+    x0 = 0
+    x1 = 2
+    assert secant(x0, x1, TOLERANCE, NMAX, ERROR, f_test_1, True) == -0.9999999997553128
+
+
 """
 # Parametros que se reciben para la ejecucion
 ecuacion = input("Ingrese la ecuacion \n ")
@@ -23,33 +69,3 @@ def dev():
 def df(x):
     return eval(dev())
 """
-
-x0 = 0
-x1 = 2
-tol = 0.00001
-nmax = 100
-
-def f(x):
-    return x ** 3 + 1
-
-def df(x):
-    return 3 * (x**2)
-
-# error y iteracion inicial
-error = 100
-ni = 0
-
-
-print("# iter\t\t x \t\t f(x) \t\t error")
-print("{0:d}\t{1:8.6f}\t{2:8.6f}\t{3:8.6f}".format(ni, x1, f(x1), error))
-
-# Ejecucion del proceso
-while error > tol and ni < nmax:
-    x2 = x1 - (f(x1) * (x1 - x0)) / (f(x1) - f(x0))
-    ni += 1
-    error = abs(x2 - x1)
-    print("{0:d}\t{1:8.4f}\t{2:8.6f}\t{3:8.6f}".format(ni, x1, x2, error))
-    x0 = x1
-    x1 = x2
-
-print("La raiz es: ", x1)
