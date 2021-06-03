a51cee1477729789c04f4f40aba4810bb78b57ae
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-06-02 20:15:47 -0500

    Fixed Point Test added

--- Python/fixedPoint.py
+++ Python/fixedPoint.py
@@ -1,6 +1,52 @@
 import numpy as np
 from math import *
 
+# Global variables
+NMAX = 20
+ERROR = 100
+TOLERANCE = 0.00001
+
+
+def fixedPoint(p0, tol, nmax, g, test):
+    iteri = 0
+    # Ejecucion
+    while iteri < nmax:
+        p = g(p0)
+        if not test:
+            print("p = \t" + str(p))
+        error = abs(p0 - p)
+        if error < tol:
+            break
+        p0 = p
+        if not test:
+            print("p0 = \t" + str(p0))
+            print()
+        iteri += 1
+
+    # Salida
+    if iteri > nmax:
+        print("El metodo no converge")
+    else:
+        if not test:
+            print("El punto de la funcion g(x) es \t" + str(p))
+        else:
+            return p
+
+
+def g_test_1(x):
+    return (2 - (np.exp(x)) + x**2) / 3
+
+
+if __name__ == "__main__":
+    p0 = 0
+    fixedPoint(p0, TOLERANCE, NMAX, g_test_1, False)
+
+
+# Tests
+def test_1():
+    p0 = 0
+    assert fixedPoint(p0, TOLERANCE, NMAX, g_test_1, True) == 0.25753180626753985
+
 
 """
 # Entradas
@@ -15,33 +61,3 @@ def g(x):
     return eval(ecuacion)
 
 """
-
-p0 = 0
-tol = 0.00001
-nmax = 100
-iteri = 0
-
-
-def g(x):
-    return (2 - (np.exp(x)) + x**2) / 3
-
-
-# Ejecucion
-while iteri < nmax:
-    p = g(p0)
-    print("p = \t" + str(p))
-    error = abs(p0 - p)
-    if error < tol:
-        break
-    p0 = p
-    print("p0 = \t" + str(p0))
-    print()
-    iteri += 1
-
-
-# Salida
-
-if iteri > nmax:
-    print("El metodo no converge")
-else:
-    print("El punto de la funcion g(x) es \t" + str(p))
